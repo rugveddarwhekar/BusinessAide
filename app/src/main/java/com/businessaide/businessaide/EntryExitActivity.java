18713e818786009a75347b9630ddfe281b6ebfbc
Rugved Darwhekar <rugveddarwhekar@gmail.com>
2017-10-28 19:17:35 +0530

    BusinessAide v_1.0.1 | Login Success with Async task. Remember to use "context" in Intent | Commit 28th Oct, 7:15pm (2)

---
+++ app/src/main/java/com/businessaide/businessaide/EntryExitActivity.java
@@ -0,0 +1,13 @@
+package com.businessaide.businessaide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntryExitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_exit);
+    }
+}
--- app/src/main/java/com/businessaide/businessaide/MainActivity.java
+++ app/src/main/java/com/businessaide/businessaide/MainActivity.java
@@ -1,13 +1,133 @@
 package com.businessaide.businessaide;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText UsernameEt, PasswordEt;
+    String flag ="0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UsernameEt = findViewById(R.id.username);
+        PasswordEt = findViewById(R.id.password);
     }
+
+    public class BackgroundWorker extends AsyncTask<String, Void, String> {
+        Context context;
+        AlertDialog alertDialog;
+
+        BackgroundWorker(Context ctx) {
+            context = ctx;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String type = params[0];
+            String login_url = "http://businessaide.co.in/login.php";
+            if (type.equals("login")) {
+                try {
+                    String user_name = params[1];
+                    String password = params[2];
+                    URL url = new URL(login_url);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(user_name, "UTF-8") + "&"
+                            + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                    String result = "";
+                    String line = "";
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result += line;
+                    }
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    return result;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            alertDialog = new AlertDialog.Builder(context).create();
+            alertDialog.setTitle("Login Status");
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+
+            if(result.equals("1"))
+            {
+
+               // Log.i("error", "flag setted "+result+"flag"+flag);
+                Intent i = new Intent(getApplicationContext(), EntryExitActivity.class);
+                startActivity(i);
+                Toast.makeText(context, "Welcome", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else if(result.equals("0"))
+            {
+
+               // Log.i("error", "flag setted "+result+"flag"+flag);
+                Toast.makeText(context, "Incorrect credentials", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
+
+    public void onLogin(View view) {
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
+
+
+    }
+
 }
