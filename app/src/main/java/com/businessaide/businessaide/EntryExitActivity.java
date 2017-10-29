af350830e998885cc48b58bff2bea50b8ee7b843
Rugved Darwhekar <rugveddarwhekar@gmail.com>
2017-10-30 00:17:39 +0530

    BusinessAide v_1.1.1 | Removed Async task from MainActivity. Implemented Volley and JSON. Double Back Pressed implemented. | Commit 30th Oct, 12:16am (4)

--- app/src/main/java/com/businessaide/businessaide/EntryExitActivity.java
+++ app/src/main/java/com/businessaide/businessaide/EntryExitActivity.java
@@ -1,26 +1,56 @@
 package com.businessaide.businessaide;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EntryExitActivity extends AppCompatActivity {
+
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_exit);
+
+        String user_name = getIntent().getExtras().getString("name_user");
+
+        TextView tv = findViewById(R.id.welcomeText);
+        tv.setText("Hello, "+user_name);
     }
 
-    public void onEntryClicked(View view){
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
 
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+    public void onEntryClicked(View view){
+        Log.d("entry", "onEntryClicked: Clicked");
     }
 
     public void onExitClicked(View view){
-
+        Log.d("exit", "onExitClicked: Clicked");
     }
 
     public void onDataClicked(View view){
+        Log.d("data", "onDataClicked: Clicked");
 
     }
 }
--- app/src/main/java/com/businessaide/businessaide/MainActivity.java
+++ app/src/main/java/com/businessaide/businessaide/MainActivity.java
@@ -20,12 +20,35 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText UsernameEt, PasswordEt;
     String flag ="0";
+    String username;
+    String password;
+
+    JSONObject jbo;
+    String user;
+    JSONArray name = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,97 +58,166 @@ public class MainActivity extends AppCompatActivity {
         PasswordEt = findViewById(R.id.password);
     }
 
-    public class BackgroundWorker extends AsyncTask<String, Void, String> {
-        Context context;
-        AlertDialog alertDialog;
-
-        BackgroundWorker(Context ctx) {
-            context = ctx;
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String type = params[0];
-            String login_url = "http://businessaide.co.in/login.php";
-            if (type.equals("login")) {
-                try {
-                    String user_name = params[1];
-                    String password = params[2];
-                    URL url = new URL(login_url);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                    httpURLConnection.setRequestMethod("POST");
-                    httpURLConnection.setDoOutput(true);
-                    httpURLConnection.setDoInput(true);
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                    String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(user_name, "UTF-8") + "&"
-                            + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
-                    bufferedWriter.write(post_data);
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
-                    outputStream.close();
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
-                    String result = "";
-                    String line = "";
-                    while ((line = bufferedReader.readLine()) != null) {
-                        result += line;
-                    }
-                    bufferedReader.close();
-                    inputStream.close();
-                    httpURLConnection.disconnect();
-                    return result;
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            alertDialog = new AlertDialog.Builder(context).create();
-            alertDialog.setTitle("Login Status");
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-
-
-            if(result.equals("1"))
-            {
-
-               // Log.i("error", "flag setted "+result+"flag"+flag);
-                Intent i = new Intent(getApplicationContext(), EntryExitActivity.class);
-                startActivity(i);
-                Toast.makeText(context, "Welcome", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-            else if(result.equals("0"))
-            {
-
-               // Log.i("error", "flag setted "+result+"flag"+flag);
-                Toast.makeText(context, "Incorrect credentials", Toast.LENGTH_SHORT).show();
-            }
-
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-    }
+//    public class BackgroundWorker extends AsyncTask<String, Void, String> {
+//        Context context;
+//        AlertDialog alertDialog;
+//
+//        BackgroundWorker(Context ctx) {
+//            context = ctx;
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            String type = params[0];
+//            String login_url = "http://businessaide.co.in/login.php";
+//            if (type.equals("login")) {
+//                try {
+//                    String user_name = params[1];
+//                    String password = params[2];
+//                    URL url = new URL(login_url);
+//                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//                    httpURLConnection.setRequestMethod("POST");
+//                    httpURLConnection.setDoOutput(true);
+//                    httpURLConnection.setDoInput(true);
+//                    OutputStream outputStream = httpURLConnection.getOutputStream();
+//                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+//                    String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(user_name, "UTF-8") + "&"
+//                            + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+//                    bufferedWriter.write(post_data);
+//                    bufferedWriter.flush();
+//                    bufferedWriter.close();
+//                    outputStream.close();
+//                    InputStream inputStream = httpURLConnection.getInputStream();
+//                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+//                    String result = "";
+//                    String line = "";
+//                    while ((line = bufferedReader.readLine()) != null) {
+//                        result += line;
+//                    }
+//                    bufferedReader.close();
+//                    inputStream.close();
+//                    httpURLConnection.disconnect();
+//                    return result;
+//                } catch (MalformedURLException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//            alertDialog = new AlertDialog.Builder(context).create();
+//            alertDialog.setTitle("Login Status");
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//
+//
+//
+//            if(result.equals("1"))
+//            {
+//
+//               // Log.i("error", "flag set "+result+"flag"+flag);
+//                Intent i = new Intent(getApplicationContext(), EntryExitActivity.class);
+//                startActivity(i);
+//                Toast.makeText(context, "Welcome", Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//            else if(result.equals("0"))
+//            {
+//
+//               // Log.i("error", "flag set "+result+"flag"+flag);
+//                Toast.makeText(context, "Incorrect credentials", Toast.LENGTH_SHORT).show();
+//            }
+//
+//        }
+//
+//        @Override
+//        protected void onProgressUpdate(Void... values) {
+//            super.onProgressUpdate(values);
+//        }
+//    }
 
     public void onLogin(View view) {
-        String username = UsernameEt.getText().toString();
-        String password = PasswordEt.getText().toString();
+        UsernameEt = findViewById(R.id.username);
+        PasswordEt = findViewById(R.id.password);
+        username = UsernameEt.getText().toString();
+        password = PasswordEt.getText().toString();
 
-        String type = "login";
-        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
-        backgroundWorker.execute(type, username, password);
+        try {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                    "http://businessaide.co.in/login.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String result) {
+                            if (!(result.equals("0"))) {
+
+                                try {
+                                    jbo = new JSONObject(result);
+                                    name = jbo.getJSONArray("result");
+                                    for(int i =0; i<name.length(); i++)
+                                    {
+                                        JSONObject c = name.getJSONObject(i);
+                                        user = c.getString("name");
+
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+
+
+
+                                Log.i("error_response", result);
+                                Intent i = new Intent(getApplicationContext(), EntryExitActivity.class);
+                                i.putExtra("name_user", user);
+                                startActivity(i);
+                                //Toast.makeText(getApplicationContext(), "Welcome "+user, Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else if (result.equals("0")) {
+
+                                // Log.i("error", "flag set "+result+"flag"+flag);
+                                Toast.makeText(getApplicationContext(), "Incorrect credentials", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+
+                        }
+                    }) {
+                public static final String TAG = "PV";
+
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    Map<String, String> params = new HashMap<>();
+                    params.put("user_name", username);
+                    params.put("password", password);
+                    return params;
+                }
+
+
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+        }catch(Exception e){
+            Log.d("e", "onLogin: Exception");
+        }
+//
+//        String type = "login";
+//        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+//        backgroundWorker.execute(type, username, password);
 
 
     }
