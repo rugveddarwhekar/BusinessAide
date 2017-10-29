1ecf36d45c1229055215d13717994cc6dcc2c3f1
Rugved Darwhekar <rugveddarwhekar@gmail.com>
2017-10-29 11:22:26 +0530

    BusinessAide v_1.1.0 | Starting with EntryExitActivity. | Commit 29th Oct, 11:21am (3)

--- app/src/main/java/com/businessaide/businessaide/EntryExitActivity.java
+++ app/src/main/java/com/businessaide/businessaide/EntryExitActivity.java
@@ -2,6 +2,7 @@ package com.businessaide.businessaide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EntryExitActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class EntryExitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_exit);
+    }
+
+    public void onEntryClicked(View view){
+
+    }
+
+    public void onExitClicked(View view){
+
+    }
+
+    public void onDataClicked(View view){
+
     }
 }
