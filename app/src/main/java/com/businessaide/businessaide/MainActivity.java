f3048a8d29c9a5da601ccc70526759b1c35e06e4
Rugved Darwhekar <rugveddarwhekar@gmail.com>
2017-10-30 00:19:48 +0530

    BusinessAide v_1.1.1 | Removed Async task from MainActivity. Implemented Volley and JSON. Double Back Pressed implemented. | Commit 30th Oct, 12:16am (4)

--- app/src/main/java/com/businessaide/businessaide/MainActivity.java
+++ app/src/main/java/com/businessaide/businessaide/MainActivity.java
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
